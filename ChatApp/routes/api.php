b80811db6f2c9529d7fe852f39cc10d4b09f3919
ostojic02 <ostojic.djordje17@gmail.com>
2025-03-07 19:11:29 +0100

    Peti commit- Kontroler i rute za rad sa chatovima

--- ChatApp/routes/api.php
+++ ChatApp/routes/api.php
@@ -3,9 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatController;
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
     Route::get('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+});
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('chat', ChatController::class)->only(['index', 'store', 'show']);
 });
